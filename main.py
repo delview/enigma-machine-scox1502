73cd60c98ef17956d06cd51494eb7b8ad0c0b8c6
scox1502 <samuelc506@deltalearns.ca>
2025-03-06 18:20:37 +0000

    started on a seprit dictopnary for decrypting and encrypting code

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ decript_phone = []
 encripted_code = []
 decripted_code = []
 
-code = {
+code_encrypt = {
     "a": "2", "b": "22", "c": "222", "d": "3", "e": "33", "f": "333", "g": "4", "h": "44", "i": "444", 
     "j": "5", "k": "55", "l": "555", "m": "6", "n": "66", "o": "666", "p": "7", "q": "77", "r": "777", 
     "s": "7777", "t": "8", "u": "88", "v": "888", "w": "9", "x": "99", "y": "999", "z": "9999"
+}
+code_decrypt = {
+    "2": "a", "22": "b", "222": "c", "3": "d", "33": "e"
 }
 
 print("hello wellcome to the encript and decripting program.")
